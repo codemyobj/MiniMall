12b8b9be95dc16a35a8acce290d9bc55148b16c5
codemyobj <459324704@qq.com>
2021-07-28 23:08:42 +0800

    完成首页的轮播图组件

---
+++ components/common/Swiper/Swiper.vue
@@ -0,0 +1,38 @@
+<template>
+	<swiper
+		circular
+		autoplay
+		indicator-dots
+		indicator-active-color="#ff5777"
+		:interval="3000"
+		:duration="500">
+		<swiper-item
+			v-for="(item,index) in banner"
+			:key='index'>
+			<view class="swiper">
+				<image
+					:src="item.image"
+					mode="widthFix"></image>
+			</view>
+		</swiper-item>
+	</swiper>
+</template>
+
+<script>
+	export default {
+		props: {
+			banner: {
+				type: Array,
+				default () {
+					return []
+				}
+			}
+		}
+	}
+</script>
+
+<style>
+	.swiper image {
+		width: 100%;
+	}
+</style>
--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,22 +1,40 @@
 <template>
 	<view>
-		
+		<swiper :banner='banner'/>
 	</view>
 </template>
 
 <script>
+	import Swiper from '@/components/common/Swiper/Swiper.vue'
+	
+	import {
+		getMultiData
+	} from '@/service/home.js'
+
 	export default {
 		data() {
 			return {
-				
+				banner: [],
+				recommend: []
 			}
 		},
-		methods: {
-			
-		}
+		components: {
+			Swiper
+		},
+		onLoad(options) {
+			getMultiData().then(res => {
+				// 取出轮播图和推荐数据
+				const data = res.data.data
+				this.banner = data.banner.list
+				this.recommend = data.recommend.list
+
+				console.log(this.banner)
+				console.log(this.recommend)
+			})
+		},
 	}
 </script>
 
 <style>
-
+	
 </style>
